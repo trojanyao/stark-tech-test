ed5d3c8ee1d63a50141b08378c52c7411d1c3324
trojan.yao <ytj1996@gmail.com>
2025-08-07 15:12:59 +0800

    build(`next.config.ts`): add `transpilePackages` for `@mui/x-charts`

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+  transpilePackages: ['@mui/x-charts']
+}
 
-export default nextConfig;
+export default nextConfig
